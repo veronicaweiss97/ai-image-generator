787ab4cca1d581c96c1c0937cae6155483c6df64
Veronica <veronicaweiss77@gmail.com>
2023-07-01 17:08:39 +0200

    initial

---
+++ main.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { engine } from 'express-handlebars'
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_, res) => {
+    res.render('index')
+})
+
+app.listen(3000, () => console.log('Server started...'))
